8da8044d2f9703cb9eeef5df349ee6efe4e6c92b
Kirtan440 <kirtant1910@gmail.com>
2026-05-01 17:34:58 +0530

    Update code

--- If-loop.py
+++ If-loop.py
@@ -11,55 +11,54 @@ else:
 
 #lab examples
 
-# var1 = True
+var1 = True
 
-# if var1:
-#     print("var1")
-# else:
-#     print("var1 is not true")
+if var1:
+    print("var1")
+else:
+    print("var1 is not true")
 
 
-# for i in range(0, 10, 1):
-#     print(i)
-# for i in range(0, 10):
-#     print(i)
-# for i in range(10):
-#     print(i)
+for i in range(0, 10, 1):
+    print(i)
+for i in range(0, 10):
+    print(i)
+for i in range(10):
+    print(i)
 
-# s = "Hello World"
+s = "Hello World"
 
-# for i in s:
-#     print('current Letter:', i)
+for i in s:
+    print('current Letter:', i)
 
-# print(range(10))
+print(range(10))
 
-# a = ['Kirtan' , 'Rydham', 'Ahmedabad']
-# print(len(a))
+a = ['Kirtan' , 'Rydham', 'Ahmedabad']
+print(len(a))
 
-# for i in range(len(a)):
-#     print(i,a[i])
+for i in range(len(a)):
+    print(i,a[i])
 
-# for i in range(20,30):
-#     print("index",i)
-# else:
-#     print("in else part")
-#     print("Hello") 
+for i in range(20,30):
+    print("index",i)
+else:
+    print("in else part")
+    print("Hello") 
 
-# ls = ["hello" for x in range (5)]
-# print(ls)
+ls = ["hello" for x in range (5)]
+print(ls)
 
-# for i in range(10):
-#     for j in range(20,30):
-#         print("j>>",j)
+for i in range(10):
+    for j in range(20,30):
+        print("j>>",j)
 
-#     print("i",i)
+    print("i",i)
 
-# count = 0
+count = 0
 
-# while count < 10:
-#     print(count)
-#     count += 1
-# else:
-#     print("count value reached to 10")
-#     print("count value is", count)
-
+while count < 10:
+    print(count)
+    count += 1
+else:
+    print("count value reached to 10")
+    print("count value is", count)
--- Taskof-if-loop.py
+++ Taskof-if-loop.py
@@ -1,59 +1,52 @@
-# x = input("enter a character: ")
+x = input("enter a character: ")
 
-# if x == 'a':
-#     print("You have enter a")
-# elif x == 'b':
-#     print("You have enter b")
-# elif x == 'c':
-#     print("You have enter c")
-# else:
-#     print("invalid character")
-
-
-
-# a = int(input("enter a number: "))
-
-# if a > 0:
-
-#     if a % 2 == 1:
-#         result = a + 5 
-#         print("Addition result is", result) 
-#         print("a is odd and positive")
-#     else:
-#         result = a * 2.5
-#         print("Multiplication result is", result)
-#         print("a is even and positive")  
-
-# if a < 0:
-
-#     if a % 2 == 1:
-#         result = a - 5 
-#         print("Subtraction result is", result) 
-#         print("a is odd and negative")
-#     else:
-#         result = a / 2.5
-#         print("Division result is", result)
-#         print("a is even and negative")     
+if x == 'a':
+    print("You have enter a")
+elif x == 'b':
+    print("You have enter b")
+elif x == 'c':
+    print("You have enter c")
+else:
+    print("invalid character")
 
 
-# marks = int(input("enter your marks: "))
-# if marks <= 100 and marks >= 90:
-#     print("Grade A")
-# elif marks < 90 and marks >= 80:
-#     print("Grade B")
-# elif marks < 80 and marks >= 60:
-#     print("Grade C")
-# elif marks < 60 and marks >= 40:
-#     print("Grade D")
-# elif marks < 40 :
-#     print("Fail")
-# else:
-#     print("Invalid marks")
 
-rows = int(input("enter number of rows: "))
+a = int(input("enter a number: "))
+
+if a > 0:
+
+    if a % 2 == 1:
+        result = a + 5 
+        print("Addition result is", result) 
+        print("a is odd and positive")
+    else:
+        result = a * 2.5
+        print("Multiplication result is", result)
+        print("a is even and positive")  
+
+if a < 0:
+
+    if a % 2 == 1:
+        result = a - 5 
+        print("Subtraction result is", result) 
+        print("a is odd and negative")
+    else:
+        result = a / 2.5
+        print("Division result is", result)
+        print("a is even and negative")     
 
 
-for i in range(1, rows + 1):
-    for j in range(i):
-        print(j, end=' ')
-    print("*")
+marks = int(input("enter your marks: "))
+if marks <= 100 and marks >= 90:
+    print("Grade A")
+elif marks < 90 and marks >= 80:
+    print("Grade B")
+elif marks < 80 and marks >= 60:
+    print("Grade C")
+elif marks < 60 and marks >= 40:
+    print("Grade D")
+elif marks < 40 :
+    print("Fail")
+else:
+    print("Invalid marks")
+
